43ed4aa314a6bf9c5b7289aaed4a1580f577993d
MGentieu <mgentieu02@gmail.com>
2023-11-22 09:40:12 +0100

    Création des fonctions basiques liées à la sauvegarde des fichiers. L'utilisateur rentre un nom et écrit, lit ou supprime un fichier de sauvegarde.

--- Gestion_des_sauvegardes.h
+++ Gestion_des_sauvegardes.h
@@ -5,4 +5,9 @@
 #ifndef PROJET_SNOOPY_GESTION_DES_SAUVEGARDES_H
 #define PROJET_SNOOPY_GESTION_DES_SAUVEGARDES_H
 
+
+void sauvegarder_fichier();
+void lire_fichier_de_sauvegarde();
+void suppr_fichier_de_sauvegarde();
+
 #endif //PROJET_SNOOPY_GESTION_DES_SAUVEGARDES_H
