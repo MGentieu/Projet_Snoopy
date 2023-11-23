5d6c858285a068d1f6cad118f77a5960a7c64c58
Franklin <148629934+Tartarhuga@users.noreply.github.com>
2023-11-23 17:36:55 +0100

    Correction légère des types dans la fonction mouvement_snoopy.c

--- mouvement_snoopy.c
+++ mouvement_snoopy.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <windows.h>
+#include "manip_affichage_console.h"
 
 int movesnoopy() {
+    short x, y, dx, dy;
+    short xavt, yavt;
     char key= '0';
-    while (key !='l'){
+    do{
+        xavt=x;
+        yavt=y;
+        //Gérons maintenant les problèmes de bordure :
+        if(x>20) x=20;
+        if(y>10) y=10;
+        if(x<0) x=0;
+        if(y<0) y=0;
+        //Gérons maintenant l'affichage de Snoopy:
+        //Premièrement, on supprime l'affichage de Snoopy :
+        goto_ligne_colonne(xavt,yavt);
+        printf(" ");
+        //On affiche Snoopy après son déplacement :
+        goto_ligne_colonne(x,y);
+        printf("%c",0x02);
+        //On note maintenant la valeur de la touche appuyée par le joueur :
         if (kbhit()){
             key = getch() ;
             switch(key){
-                case 'z':
+                case 'z': dx=0;dy=1;
                     break;
-                case 'q':
+                case 'q': dx=-1;dy=0;
                     break;
-                case 's':
+                case 's': dx=0;dy=-1;
                     break;
-                case 'd':
+                case 'd': dx=1;dy=0;
                     break;
             }
         }
-    }
-
+    }while (key !='l');
     return 0;
 }
