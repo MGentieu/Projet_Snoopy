5b628f168e37ddd2dd12de954ef794d04f388a9b
MGentieu <mgentieu02@gmail.com>
2023-11-26 15:43:10 +0100

    Implémentation d'une fonction qui finit le niveau si on a récupéré les 4 oiseaux.

--- Gestion_du_jeu.h
+++ Gestion_du_jeu.h
@@ -15,6 +15,7 @@ int movesnoopy(Plateau *);
 int verif_collisions(Plateau *,unsigned char, unsigned char);
 int verif_banane(Plateau * ptPlateau,unsigned char X_test, unsigned char Y_test);
 void up_score_car_niveau_fini(Plateau *);
+int verif_fin_niveau(Plateau *);
 // Fonction qui verifie si les blocs collant snoopy sont cassable et les cassent quand 'b' est exécuté
 void bloc_cassable(Plateau* ptPlateau);
 // Fonction balle qui prend en arguments le pointeur plateau, verifie les collisions et redirige la balle en fonctions des collisions Modification matricielle et console
