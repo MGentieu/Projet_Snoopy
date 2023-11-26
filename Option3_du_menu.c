5188c262f5f8d0b6390c8a664c465ccc83d0d1ce
Franklin <148629934+Tartarhuga@users.noreply.github.com>
2023-11-26 23:11:28 +0100

    Fermeture du fichier corrigée

--- Option3_du_menu.c
+++ Option3_du_menu.c
@@ -55,7 +55,5 @@ void affichage_option_3(){
         }
         fclose(fichier_score);
     }
-    fclose(fichier_score);
-
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN| FOREGROUND_BLUE| FOREGROUND_INTENSITY);
 }
