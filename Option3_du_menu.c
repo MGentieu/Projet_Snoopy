f913ab035f4b5da73d9819f88da40f0711963efb
Keethan <keethan.sathiyamoorthy@edu.ece.fr>
2023-11-26 20:26:30 +0100

    Scores V1

--- Option3_du_menu.c
+++ Option3_du_menu.c
@@ -5,6 +5,12 @@
 #include <Windows.h>
 
 void affichage_option_3(){
+    char score[100];
+    FILE *fichier_score = fopen("scores.txt", "r");
+    if (fichier_score != NULL) {
+        fgets(score, sizeof(score), fichier_score);
+        fclose(fichier_score);
+    }
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN| FOREGROUND_INTENSITY);
     printf(R"EOF(    ___     ___     ___     ___     ___     ___            ,-~~-.___.
@@ -15,4 +21,8 @@ _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|            ====           //
 "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'            /  \-'~;    /~~~(O)
 )EOF");
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN| FOREGROUND_BLUE| FOREGROUND_INTENSITY);
-    }
+    printf("\n\n");
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    printf("%s\n", score);
+
+}
